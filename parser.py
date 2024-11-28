145089aa45274f2938327eb6af9101322021bc41
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-11-28 17:41:04 +0100

    Parse the agenda of CAF Montpellier and generate ical

---
+++ parser.py
@@ -0,0 +1,287 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+import urllib.request
+
+image_url = 'https://extranet-clubalpin.com/app/out/'
+
+def parse_html(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    
+    sortie_liste = soup.find('div', {'id': 'sortie_liste'})
+    sortie_array = sortie_liste.findAll('div', {'class': 'sortie'})
+    print(sortie_array)
+    
+    results = []
+    for sortie in sortie_array:
+        print('========')
+        intitule_tag = sortie.find('div', {'class': 'intitule'})
+        # strip title from link 
+        title = intitule_tag.find('a').text.strip()
+        # get data-sortie-id from a tag
+        sortie_id = intitule_tag.find('a')['data-sortie-id']
+
+        inscription_url = ''
+        inscription_url_tag = sortie.find('a', {'class': 'packClub'})
+        if (inscription_url_tag):
+            inscription_url = inscription_url_tag['href']
+
+        # get text from intitule_tag after the title div
+        date_str = intitule_tag.next_sibling.strip()
+        date_start = ""
+        date_end = ""
+        # convert date to ical format
+        if date_str.startswith('le'): # same day
+            # Date format is one of the following:
+            # date_str:  le 09/01/2025 de 09:45 à 16:30
+            # date_str:  le 09/01/2025
+            # make date look like 09/01/2025, 09:45 and 09/01/2025, 16:30
+            date_arr = date_str.strip('le ').split(' de ')
+            if(len(date_arr) == 1): # No hours provided
+                date_start_str = date_arr[0] + ', 00:00'
+                date_end_str = date_arr[0] + ', 23:59'
+            else: # Start and end hours provided
+                date_start_str = date_arr[0] + ', ' + date_arr[1].split(' à ')[0]
+                date_end_str = date_arr[0] + ', ' + date_arr[1].split(' à ')[1]
+            date_start = datetime.strptime(date_start_str, "%d/%m/%Y, %H:%M")
+            date_end = datetime.strptime(date_end_str, "%d/%m/%Y, %H:%M")
+        else: # date range
+            date_arr = date_str.split('du ')[1].split(' au ')
+            date_start_arr = ', '.join(date_arr[0].split("  à "))
+            date_end_arr = ', '.join(date_arr[1].split(" à "))
+            print(date_start_arr)
+            print(date_end_arr)
+            # now we get date with the following format
+            # date_start_arr = '08/02/2025, 07:00'
+            # parse date into ical format
+            date_start = datetime.strptime(date_start_arr, "%d/%m/%Y, %H:%M")
+            date_end = datetime.strptime(date_end_arr, "%d/%m/%Y, %H:%M")
+    
+        activite_image_tag = sortie.find('img', {'class': 'activite'})
+        activite_image = image_url + activite_image_tag['src']
+        # get parent div
+        parent = activite_image_tag.parent
+        activite_nom = parent.text.strip()
+        # get lieu
+        lieu = sortie.find('div', {'class': 'lieu'}).text.strip()
+    
+        # Find div with containing text 'Difficulté'
+        # Difficulté can either be:
+        # - Tout niveaux
+        # - or two images corresponding to:
+        #   - Niveau physique
+        #   - Niveau technique
+    
+        difficulte_tag = sortie.find('div', text='Difficulté')
+        difficulte = ""
+        niveau_technique=""
+        niveau_physique=""
+        niveau_technique_img=""
+        niveau_physique_img=""
+        # get img tag with title "Niveau physique"
+        niveau_physique_tag = difficulte_tag.find_next_sibling('img', {'title': 'Niveau physique'})
+        if niveau_physique_tag:
+            niveau_technique_tag = difficulte_tag.find_next_sibling('img', {'title': 'Niveau technique'})
+            niveau_technique_img = image_url + niveau_technique_tag['src']
+            niveau_physique_img = image_url + niveau_physique_tag['src']
+            print("Niveau physique: ", niveau_physique_img)
+            print("Niveau technique: ", niveau_technique_img)
+    
+            NIVEAU_PHYSIQUE_MAP = {
+                '42': '4',
+                '38': '3',
+                '34': '2',
+                '30': '1',
+                '26': '1',
+            }
+            NIVEAU_TECHNIQUE_MAP = {
+                '34': '4',
+                '30': '3',
+                '26': '2',
+                '22': '1',
+                '18': '1'
+            }
+    
+            # url is image.php?type=physique&amp;id=30 
+            # get id from url
+            niveau_physique_id = niveau_physique_img.split('id=')[1]
+            niveau_physique = NIVEAU_PHYSIQUE_MAP.get(niveau_physique_id, "Inconnu: " + niveau_physique_img)
+            niveau_technique_id = niveau_technique_tag['src'].split('id=')[1]
+            niveau_technique = NIVEAU_TECHNIQUE_MAP.get(niveau_technique_id, "Inconnu: " + niveau_technique_img)
+        else:
+            difficulte = difficulte_tag.next_sibling.strip()
+            niveau_technique = difficulte 
+            niveau_physique = difficulte
+    
+        denivele_tag = sortie.find_next('div', {'class': 'denivele'})
+        denivele = ""
+        if denivele_tag:
+            denivele = denivele_tag.text.strip()
+            # ensure there is at most a single space
+            denivele = ' '.join(denivele.split())
+            print(denivele)
+    
+        text = [x.strip() for x in sortie.text.split('\n')]
+        # find 'Responsable :' and next element in list
+        responsable = text[text.index('Responsable :')+1]
+    
+        places = ""
+        # Places can be:
+        # - Capacite illimitée
+        # - 9/12 inscriptions confirmées
+        def isValInLst(val, lst):
+            return [index for index, content in enumerate(lst) if val in content]
+    
+        if isValInLst('Capacite illimitée', text):
+            places = 'Capacite illimitée'
+        elif isValInLst('inscriptions confirmées', text):
+            id = isValInLst('inscriptions confirmées', text)[0]
+            places = text[id]
+            if not places.endswith('inscriptions confirmées'):
+                places = places.replace('inscriptions confirmées', 'inscriptions confirmées, ')
+            if not places.endswith('Capacite illimitée'):
+                places = places.replace('Capacité illimitée', 'Capacité illimitée, ')
+    
+        STATUS = ["AU PLANNING", "VALIDEE", "ANNULEE"]
+        # find if status is in text list
+        status = [x for x in STATUS if x in text]
+        if status:
+            status = status[0]
+        else:
+            status = 'INCONNU'
+    
+    
+        dict = {
+            'title': title,
+            'sortie_id': sortie_id,
+            'inscription_url': inscription_url,
+            'activite_image': activite_image,
+            'activite_nom': activite_nom,
+            'lieu': lieu,
+            'date_str': date_str,
+            'date_start': date_start,
+            'date_end': date_end,
+            'difficulte': difficulte,
+            'niveau_technique': str(niveau_technique),
+            'niveau_technique_img': niveau_technique_img,
+            'niveau_physique': str(niveau_physique),
+            'niveau_physique_img': niveau_physique_img,
+            'denivele': denivele,
+            'places': places,
+            'responsable': responsable,
+            'status': status
+        }
+        results.append(dict)
+
+    return results
+
+def printSorties(results):
+    for r in results:
+        print('title: ', r['title'])
+        print('sortie_id: ', r['sortie_id'])
+        print('inscription_url: ', r['inscription_url'])
+        print('activite_image: ', r['activite_image'])
+        print('activite_nom: ', r['activite_nom'])
+        print('lieu: ', r['lieu'])
+        print('date_str: ', r['date_str'])
+        print('date start: ', r['date_start'])
+        print('date end: ', r['date_end'])
+        print('difficulte: ', r['difficulte'])
+        print('niveau_technique: ', r['niveau_technique'])
+        print('niveau_physique: ', r['niveau_physique'])
+        if(r['niveau_technique_img']):
+            print('niveau_technique_img: ', r['niveau_technique_img'])
+        print('niveau_physique: ', r['niveau_physique'])
+        if(r['niveau_physique_img']):
+            print('niveau_physique_img: ', r['niveau_physique_img'])
+        print('denivele: ', r['denivele'])
+        print('places: ', r['places'])
+        print('responsable: ', r['responsable'])
+        print('status: ', r['status'])
+
+    
+    # for event in events:
+    #     title = event.text.strip()
+    #     date_str = event.find_next_sibling('p').text.strip()  # Find the next paragraph
+    #
+    #     # Extract date (assuming it's the first part before "de")
+    #     date_str = date_str.split('de').strip()
+    #
+    #     # Parse date string
+    #     try:
+    #         date_obj = datetime.strptime(date_str, "%d/%m/%Y")
+    #         date_ical = date_obj.strftime("%Y%m%d")
+    #     except ValueError:
+    #         print(f"Skipping event '{title}' due to invalid date format: {date_str}")
+    #         continue
+    #
+    #     print("BEGIN:VEVENT")
+    #     print(f"SUMMARY:{title}")
+    #     print(f"DTSTART:{date_ical}")
+    #     print(f"DTEND:{date_ical}")
+    #     print("END:VEVENT")
+    #
+    # print("END:VCALENDAR")
+
+def genICAL(results, prefix="[CAF MPL]"):
+    ical_str = ""
+    ical_str += "BEGIN:VCALENDAR\n"
+    ical_str += "VERSION:2.0\n"
+    for r in results:
+        # format date and hour in ical
+        date_start_ical = r['date_start'].strftime("%Y%m%dT%H%M%S")
+        date_end_ical = r['date_end'].strftime("%Y%m%dT%H%M%S")
+        description = "Sortie: " + r['title'] + "\\nLieu: " + r['lieu'] + "\\nNiveau Technique: " + r['niveau_technique'] + "\\nNiveau Physique: " + r['niveau_physique'] + "\\n" + r['denivele'] + "\\nPlaces: " + r['places'] + "\\nResponsable: " + r['responsable'] + "\\nStatus: " + r['status']
+        if r['inscription_url']:
+            description += "\\nInscription: " + r['inscription_url']
+
+        ical_str += "BEGIN:VEVENT\n"
+        # set UID
+        ical_str += "UID:"+r['sortie_id']+"\n"
+        timestamp = int(time.time())
+        ical_str += "SEQUENCE:" + str(timestamp) + "\n" # Always increasing sequence number such that each update to this event with the same UID is propagated
+        ical_str += "SUMMARY:"+prefix + " " + r['title']+"\n"
+        ical_str += "DTSTART:"+date_start_ical+"\n"
+        ical_str += "DTEND:"+date_end_ical+"\n"
+        ical_str += "LOCATION:"+r['lieu']+"\n"
+        ical_str += "DESCRIPTION:"+description+"\n"
+        # organiser
+        ical_str += "ORGANIZER;CN="+r['responsable']+":\n"
+        status = ""
+        if r['status'] == 'AU PLANNING':
+            status = "TENTATIVE"
+        elif r['status'] == 'VALIDEE':
+            status = "CONFIRMED"
+        elif r['status'] == 'CANCELLED':
+            status = "CANCELLED"
+        ical_str += "STATUS:"+status+"\n"
+        ical_str += "END:VEVENT\n"
+    ical_str += "END:VCALENDAR\n"
+    return ical_str
+
+# main function
+def main():
+    # read from example.html
+    # html_content = open('example.html', 'r').read()
+
+    agenda_url = 'https://extranet-clubalpin.com/app/out/out.php?s=12&c=3400&h=32cdfd3f91'
+    response = urllib.request.urlopen(agenda_url)
+    html_content = response.read().decode('utf-8')
+
+    results = parse_html(html_content)
+    # results = results[0:5] 
+    print ("====================")
+    print ("====================")
+    print ("====================")
+    print ("====================")
+    print('Number of sorties: ', len(results))
+    print(results)
+    printSorties(results)
+    ical_str = genICAL(results)
+    # write ical to tmp
+    with open('/tmp/sorties_caf.ical', 'w') as f:
+        f.write(ical_str)
+
+
+if __name__ == "__main__":
+    main()
